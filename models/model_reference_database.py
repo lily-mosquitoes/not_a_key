a9d7d02acd570b556d6db4ff69a411e4c6fd06a5
Lily <lily.mosquitoes@gmail.com>
2020-09-30 08:13:35 -0300

    fixed abundance calculation (fixes #1), changed code formatting

--- models/model_reference_database.py
+++ models/model_reference_database.py
@@ -6,6 +6,7 @@ import sqlite3
 class ReferenceDatabase(object):
 
     def __init__(self, path):
+
         self._connection = sqlite3.connect(path)
         self.cursor = self._connection.cursor()
 
@@ -40,54 +41,72 @@ class ReferenceDatabase(object):
         return self._connection
 
     def get_locations(self):
+
         sql = "SELECT state FROM species_data;"
+
         try:
             self.cursor.execute(sql)
             locations = self.cursor.fetchall()
             locations = [i[0] for i in locations]
             locations = sorted(set(locations))
             locations.remove('') # remove "" (empty) state (no state info)
+
         except sqlite3.Error as e:
             raise e
+
         return locations
 
 
     def _get_species_entries(self, state):
+
         if state == 'all states':
             sql = "SELECT * FROM species_data;"
         else:
             sql = f"SELECT * FROM species_data WHERE state = '{state}';"
-        #
+
         try:
             self.cursor.execute(sql)
             species_entries = self.cursor.fetchall()
         except sqlite3.Error as e:
             raise e
-        #
+
         return species_entries
 
     def _filter_entries(self, location, sortby, sortby2):
+
         raw_entry_data = self._get_species_entries(location)
         # raw_entry_data == [species, state, month, count]
-        #
+
         entries = defaultdict(int)
+
         # if all months selected, return all entries
         if sortby == 'full year':
             for entry in raw_entry_data:
-                species, count = entry[0], entry[3]
+
+                species = entry[0]
+                count = entry[3]
+
                 entries[species] += int(count)
+
             return entries
+
         # else, return filtered entries
         elif sortby == 'season' or sortby == 'month':
+
+            # sortlist gets season or month
             sort_list = self.sort_list_alias[sortby]
+
             for entry in raw_entry_data:
+
                 month = entry[2] # months are: MM
                 species = entry[0]
                 count = entry[3]
+
                 if month in sort_list[sortby2]:
                     entries[species] += int(count)
                 else:
                     entries[species] += 0
+
             return entries
 
     def get_abundance_matrix(self, location, sortby, sortby2):
@@ -97,12 +116,22 @@ class ReferenceDatabase(object):
         that it will show species with zero abundance. This is
         important to catch new records / shifting occurrence
         """
-        #
+
         entries = self._filter_entries(location, sortby, sortby2)
-        #
-        total_individuals = sum([i[1] for i in entries.items()])
-        #
+
+        # total individuals for the selected time frame
+        # if total is 0 no individuals of any species have been caught
+        # in that time frame; if so, use 1 for calculation
+        total_individuals = sum([i[1] for i in entries.items()]) or 1
+
         for sp, count in entries.items():
+
+            # this is the relative abundance of each species
+            # regarding the species present in that time frame;
+            # species with 0 individuals in that time frame will
+            # get 0 abundance; if no individuals are present in that
+            # time frame, use any number (here, 1) to divide all 0 counts
+            # and still show a percentage of zero
             entries[sp] = round((count/total_individuals)*100, 2)
-        #
+
         return entries
